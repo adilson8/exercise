45994f405ac50f641bd97ffaa4c5369760f4b173
adilson8 <adilson8@naver.com>
2022-05-13 20:25:13 +0900

    go

--- exercise/src/exercise/BoxExample.java
+++ exercise/src/exercise/BoxExample.java
@@ -5,7 +5,7 @@ public class BoxExample {
 	
 	public static void main(String[] args) {
 		Box<String> box1 = new Box<String>();
-		box1.set("hello");
+		box1.set("hello!");
 		String str = box1.get();
 
 		Box<Integer> box2 = new Box<Integer>();
