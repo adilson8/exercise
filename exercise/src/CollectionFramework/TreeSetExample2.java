1ead94680394684ba4022e1d6a8fd50bb402046d
adilson8 <adilson8@naver.com>
2022-05-31 21:46:31 +0900

    TreeSet

--- exercise/src/CollectionFramework/TreeSetExample2.java
+++ exercise/src/CollectionFramework/TreeSetExample2.java
@@ -7,22 +7,27 @@ public class TreeSetExample2 {
 	public static void main(String[] args) {
 		TreeSet<Integer> scores = new TreeSet<Integer>();
 		
-		scores.add(87);
+		scores.add(87); // auto-boxing
 		scores.add(98);
 		scores.add(75);
 		scores.add(95);
 		scores.add(80);
 		
+		// 내림차순으로 정리결과를 Set으로 변환해주는 메소드(descendingSet) 호출
 		NavigableSet<Integer> descendingSet = scores.descendingSet();
+		
 		for(Integer score : descendingSet) {
 			System.out.print(score + " ");
-		}
-		System.out.println();
+		} // enhanced for
 		
+		System.out.println();		
+		
+		// 오름차순으로 정렬결과를 Set으로 반환해주는 메소드(descendingSet) 호출
 		NavigableSet<Integer> ascendingSet = descendingSet.descendingSet();
 		
 		for(Integer score : ascendingSet) {
 			System.out.print(score + " ");
-		}
-	}
-}
+		} // enhanced for
+	} // main
+	
+} // end class
--- exercise/src/CollectionFramework/TreeSetExample3.java
+++ exercise/src/CollectionFramework/TreeSetExample3.java
@@ -6,6 +6,7 @@ import java.util.TreeSet;
 public class TreeSetExample3 {
 	public static void main(String[] args) {
 		TreeSet<String> treeSet = new TreeSet<String>();
+		
 		treeSet.add("apple");
 		treeSet.add("forever");		
 		treeSet.add("description");
@@ -14,12 +15,15 @@ public class TreeSetExample3 {
 		treeSet.add("base");
 		treeSet.add("guess");
 		treeSet.add("cherry");
+
+		// 범위검색 (Range Scan) 수행
+		System.out.println("[cherry ~ forever 사이의 단어 검색]");
 		
-		System.out.println("[c~f 사이의 단어 검색]");
-		NavigableSet<String> rangeSet = treeSet.subSet("c", true, "f", true);
+		NavigableSet<String> rangeSet = treeSet.subSet("cherry", true, "forever", true);
+		
 		for(String word : rangeSet) {
 			System.out.println(word);
-		}
-
-	}
-}
+		} // for
+	} // main
+	
+} // end class
