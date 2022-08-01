5837606063a7c29fc5d6249be01380ff81bdcab2
adilson8 <adilson8@naver.com>
2022-08-02 04:41:46 +0900

    Q03

---
+++ exercise/src/CodeUp100/Q03.java
@@ -0,0 +1,11 @@
+package CodeUp100;
+
+public class Q03 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("World");
+
+	} // main
+
+} // end class
