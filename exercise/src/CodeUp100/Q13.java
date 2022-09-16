e649b9e87a660b1123e0d38de0a41cbfa3b0cd90
adilson8 <adilson8@naver.com>
2022-09-16 23:47:00 +0900

    Q13

---
+++ exercise/src/CodeUp100/Q13.java
@@ -0,0 +1,18 @@
+package CodeUp100;
+
+import java.util.Scanner;
+
+public class Q13 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		char c1 = scan.next().charAt(0);
+		char c2 = scan.next().charAt(0);
+		
+		System.out.println(c2);
+		System.out.println(c1);
+
+	} // main
+
+} // end class
