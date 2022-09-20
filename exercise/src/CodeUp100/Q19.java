9bd23d1622631920c627407944d719b359958e94
adilson8 <adilson8@naver.com>
2022-09-20 23:56:34 +0900

    CodeUp 100

---
+++ exercise/src/CodeUp100/Q19.java
@@ -0,0 +1,22 @@
+package CodeUp100;
+
+import java.util.Scanner;
+
+public class Q19 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String time = scan.nextLine();
+		String [] arr = time.split("\\.");
+		
+		int year = Integer.valueOf(arr[0]);
+		int month = Integer.valueOf(arr[1]);
+		int day = Integer.valueOf(arr[2]);
+		
+		System.out.printf("%d.%02d.%02d", year, month, day);
+
+	} // main
+
+} // end class
