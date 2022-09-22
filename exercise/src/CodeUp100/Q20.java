f37368d8463ba73b4018fc28a424bf5e1c078150
adilson8 <adilson8@naver.com>
2022-09-23 00:02:45 +0900

    CodeUp 100

--- exercise/src/CodeUp100/Q20.java
+++ exercise/src/CodeUp100/Q20.java
@@ -6,7 +6,13 @@ public class Q20 {
 
 	public static void main(String[] args) {
 
-	
+		Scanner scan = new Scanner(System.in);
+		
+		String cdn = scan.nextLine();
+		cdn = cdn.replaceAll("-", "");
+		
+		System.out.printf(cdn);
+		
 	} // main
 
 } // end class
