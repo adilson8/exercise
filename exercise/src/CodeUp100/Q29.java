ae15c87725d3286f7f796de24bd85293180edde1
adilson8 <adilson8@naver.com>
2022-09-28 19:00:20 +0900

    CodeUp 100

---
+++ exercise/src/CodeUp100/Q29.java
@@ -0,0 +1,17 @@
+package CodeUp100;
+
+import java.util.Scanner;
+
+public class Q29 {
+
+	public static void main(String[] args) {
+
+       Scanner scan = new Scanner(System.in);
+       
+       double num = scan.nextDouble();
+       
+       System.out.printf("%.11f", num);
+    
+	} // main
+
+} // end class
