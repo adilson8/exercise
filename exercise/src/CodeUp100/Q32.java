4df22fb560828cb851dff2e19aaee6eb06cf27a7
adilson8 <adilson8@naver.com>
2022-09-30 13:50:13 +0900

    CodeUp 100

---
+++ exercise/src/CodeUp100/Q32.java
@@ -0,0 +1,17 @@
+package CodeUp100;
+
+import java.util.Scanner;
+
+public class Q32 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		 
+		String num =Integer.toHexString(scan.nextInt());
+		
+		System.out.println(num);		
+    
+	} // main
+
+} // end class
