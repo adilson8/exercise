f9c9ea517f2feb84faf1e4d571706f58b6d50c48
adilson8 <adilson8@naver.com>
2022-09-30 13:53:09 +0900

    CodeUp 100

---
+++ exercise/src/CodeUp100/Q33.java
@@ -0,0 +1,17 @@
+package CodeUp100;
+
+import java.util.Scanner;
+
+public class Q33 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		 
+		String num =Integer.toHexString(scan.nextInt()).toUpperCase();
+		
+		System.out.println(num);		
+    
+	} // main
+
+} // end class
