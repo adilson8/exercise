c39146ab9b322effc900ff5633a250434f4d04b6
adilson8 <adilson8@naver.com>
2022-10-03 00:44:18 +0900

    CodeUp 100

---
+++ exercise/src/CodeUp100/Q38.java
@@ -0,0 +1,20 @@
+package CodeUp100;
+
+import java.util.Scanner;
+
+public class Q38 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);	
+
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		
+		long sum = (long) num1 + num2;
+		
+		System.out.println(sum);
+    
+	} // main
+
+} // end class
