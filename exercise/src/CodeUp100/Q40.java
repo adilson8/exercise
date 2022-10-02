6a579283689dfc0b51f62ff10818dbbd89301af2
adilson8 <adilson8@naver.com>
2022-10-03 01:05:31 +0900

    CodeUp 100

---
+++ exercise/src/CodeUp100/Q40.java
@@ -0,0 +1,19 @@
+package CodeUp100;
+
+import java.util.Scanner;
+
+public class Q40 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);	
+
+		int a = scan.nextInt();
+		
+		int b = -a;
+		
+		System.out.println(b);
+    
+	} // main
+
+} // end class
