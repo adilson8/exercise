df14314507d841667c4abe12ab82ed306def7b1b
donghoon <donghoon@taehwainno.com>
2023-04-24 15:52:31 +0900

    AddDigits

---
+++ exercise/src/programmers/lv1/AddDigits.java
@@ -0,0 +1,18 @@
+package programmers.lv1;
+
+public class AddDigits {
+    public int solution(int n) {        
+        int answer = 0;
+		
+		String stringNum = Integer.toString(n);
+		String[] arr = stringNum.split("");
+
+		for(String num : arr) {			
+			answer = answer + Integer.parseInt(num);
+		}
+		
+		System.out.println(answer);
+        
+        return answer;
+    }
+}
