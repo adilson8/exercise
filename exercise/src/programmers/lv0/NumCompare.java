7ad132692cf6835ef02a14231f9b68f5596ab76f
donghoon <donghoon@taehwainno.com>
2023-04-24 11:16:39 +0900

    first commit

---
+++ exercise/src/programmers/lv0/NumCompare.java
@@ -0,0 +1,17 @@
+package programmers.lv0;
+
+public class NumCompare {	
+	
+	public int solution(int num1, int num2) {
+		int answer;
+		
+		if (num1 == num2) {
+			answer = 1;
+		} else {
+			answer = -1;
+		}
+		
+		return answer;
+	}
+
+}
