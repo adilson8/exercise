786d328a3e62d07dda520898512c1035cb10307f
donghoon <donghoon@taehwainno.com>
2023-04-24 16:01:50 +0900

    AddDivisors

---
+++ exercise/src/programmers/lv1/AddDivisors.java
@@ -0,0 +1,25 @@
+package programmers.lv1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddDivisors {
+
+	public int solution(int n) {
+        int answer = 0;
+
+		List<Integer> list = new ArrayList<>();
+		for(int i = 1; i <= n; i++){
+			if (n % i == 0){
+				list.add(i);
+			}
+		}
+		
+		for(int num : list) {
+			answer = answer + num;
+		}
+				
+		return answer;
+    }
+	
+}
