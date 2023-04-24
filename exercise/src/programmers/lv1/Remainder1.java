4b903590a3e6106dade23711e0c974c062b367fc
donghoon <donghoon@taehwainno.com>
2023-04-24 16:25:21 +0900

    Remainder1

---
+++ exercise/src/programmers/lv1/Remainder1.java
@@ -0,0 +1,18 @@
+package programmers.lv1;
+
+public class Remainder1 {
+	
+	public int solution(int n) {
+        int answer = 0;
+		
+		for (int i = 1; i <= n; i++){
+			if (n % i == 1){
+				answer = i;
+				break;
+			}
+		}
+		
+		return answer;
+    }
+
+}
