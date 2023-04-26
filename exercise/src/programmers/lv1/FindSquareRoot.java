1bc7a321e60536f904413d659b9069cb27a39a4f
donghoon <adilson8@naver.com>
2023-04-26 09:21:42 +0900

    FindSquareRoot

---
+++ exercise/src/programmers/lv1/FindSquareRoot.java
@@ -0,0 +1,26 @@
+package programmers.lv1;
+
+public class FindSquareRoot {
+	
+	public long solution(long n) {
+        long answer = 0;
+        
+		for (long i = 1; i <= (n); i++){
+            if (i*i > n){
+			    break;
+		    }
+            
+			if (i*i == n){
+				answer = (i+1) * (i+1);
+				break;
+			}
+		}
+		
+		if (answer == 0) {
+			answer = -1;
+		}
+        
+        return answer;
+    }
+
+}
