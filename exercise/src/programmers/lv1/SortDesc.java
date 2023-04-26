c148a381c10c853cc0b1abf0fd0de72db9e816ff
donghoon <adilson8@naver.com>
2023-04-26 14:22:06 +0900

    StringBuffer.reverse()

--- exercise/src/programmers/lv1/SortDesc.java
+++ exercise/src/programmers/lv1/SortDesc.java
@@ -1,33 +1,21 @@
 package programmers.lv1;
 
+import java.util.Arrays;
+
 public class SortDesc {
 	
 	public long solution(long n) {
         long answer = 0;
 				
-		String[] arrStrN = Long.toString(n).split("");
-		int[] arrN = new int[arrStrN.length];
-		for(int i = 0; i < arrStrN.length; i++){
-			arrN[i] = Integer.parseInt(arrStrN[i]);
-		}
+        String[] strArrN = Long.toString(n).split("");
+		Arrays.sort(strArrN);
 		
-		// 2. 1의 과정을 배열 길이만큼 반복
-		for(int num :arrN){
-			// 1. 첫번째 요소 잡고 맨 끝으로 보냄
-			for(int i =0; i < arrN.length-1; i++){
-				if(arrN[i] < arrN[i+1]){
-					int temp = arrN[i+1];
-					arrN[i+1] = arrN[i];
-					arrN[i] = temp;
-				}
-			}
+		StringBuffer sb = new StringBuffer();
+		for (String element : strArrN){
+			sb.append(element);
 		}
-		
-		String temp = "";
-		for (int i = 0; i < arrN.length; i++){
-			temp = temp + arrN[i];
-		}		
-		answer = Long.parseLong(temp);
+			
+		answer = Long.parseLong(sb.reverse().toString());
         
         return answer;
     }
