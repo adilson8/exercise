1108d068c149e4a1f78f45695d57826b6cdd88a1
donghoon <adilson8@naver.com>
2023-04-28 17:40:58 +0900

    Watermelon

---
+++ exercise/src/programmers/lv1/Watermelon.java
@@ -0,0 +1,19 @@
+package programmers.lv1;
+
+public class Watermelon {
+	
+	public String solution(int n) {
+        String answer = "";
+
+        for (int i = 1; i <= n; i++){
+            if (i % 2 != 0){
+                answer = answer + "수";
+            } else {
+                answer = answer + "박";
+            }
+        }
+
+        return answer;
+    }
+
+}
