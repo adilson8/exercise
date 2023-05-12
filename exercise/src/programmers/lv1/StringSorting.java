a1eca016302c7a5bf93f602ed75e6870dd95273f
donghoon <adilson8@naver.com>
2023-05-12 17:38:44 +0900

    StringSorting 진행중 ㅜㅜ

---
+++ exercise/src/programmers/lv1/StringSorting.java
@@ -0,0 +1,43 @@
+package programmers.lv1;
+
+import java.util.Arrays;
+
+public class StringSorting {
+
+	public static void main (String[] args){
+		
+		String[] strings = {"abce", "abcd", "cdx"};
+		int n = 1;
+		
+		for (int i =0; i < strings.length; i++){
+			int[] ints = new int[strings.length];
+			for (int k =0; k < strings.length; k++){
+				String e = strings[k];
+				char a  = e.toCharArray()[n];
+				ints[k] = (int) a;
+			}
+			
+			for (int j =0; j < strings.length; j++){
+				String temp = "";
+				try {
+					if (ints[j] > ints[j+1]){
+						temp = strings[j];
+						strings[j] = strings[j+1];
+						strings[j+1] = temp;
+					} else if (ints[j] == ints[j+1]){
+						// 조졌다 여기서 이제 사전식배열? 해야한다
+						for (int l = n; l < strings[j].length(); l++){
+							
+						}
+					}
+				} catch (ArrayIndexOutOfBoundsException e) {
+					break;
+				}			
+			}			
+		}
+		
+		System.out.println(Arrays.toString(strings));
+		
+	}
+
+}
