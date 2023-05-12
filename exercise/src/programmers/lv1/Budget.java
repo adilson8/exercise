925a87969b00991e2b6a2894d862195ec96facfa
donghoon <adilson8@naver.com>
2023-05-12 11:19:59 +0900

    Budget

--- exercise/src/programmers/lv1/Budget.java
+++ exercise/src/programmers/lv1/Budget.java
@@ -1,9 +1,24 @@
 package programmers.lv1;
 
+import java.util.Arrays;
+
 public class Budget {
 	
 	public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int sum = 0;
         int answer = 0;
+        
+        for (int e : d) {
+            sum += e;
+
+            if (sum > budget) {
+                break;
+            }
+
+            answer++;
+        }
+
         return answer;
     }
 
