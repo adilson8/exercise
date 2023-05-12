92f6c3c13f06640ed57ae375fdbdfb75cb0ceb58
donghoon <adilson8@naver.com>
2023-05-12 11:20:22 +0900

    Budget

--- exercise/src/programmers/lv1/Budget.java
+++ exercise/src/programmers/lv1/Budget.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class Budget {
 	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12982
+	
 	public int solution(int[] d, int budget) {
         Arrays.sort(d);
         int sum = 0;
