f0e2a323b17b4d27fc9ad57853064975e869704d
donghoon <adilson8@naver.com>
2023-05-15 17:44:12 +0900

    NumberPlay

---
+++ exercise/src/programmers/lv1/NumberPlay.java
@@ -0,0 +1,12 @@
+package programmers.lv1;
+
+public class NumberPlay {
+	
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/81301
+	public int solution(String s) {
+        int answer = 0;
+        return answer;
+    }
+
+}
