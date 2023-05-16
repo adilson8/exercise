42464fc2df7e93ef53342460d7a35a153064cd70
donghoon <adilson8@naver.com>
2023-05-16 17:54:49 +0900

    NumberK

---
+++ exercise/src/programmers/lv1/NumberK.java
@@ -0,0 +1,23 @@
+package programmers.lv1;
+
+import java.util.Arrays;
+
+public class NumberK {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/42748
+	public static void main (String[] args){
+		int[] array = {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+		
+		int[] answer = new int[commands.length];
+		
+		for(int[] command : commands){
+			for (int i = 0; i < command.length; i++){
+				
+			}
+		}
+		
+		System.out.println(Arrays.toString(answer));
+	}
+
+}
