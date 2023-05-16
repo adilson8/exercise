fee4d66b61bbe12fa3e4fa22c8a9941280cfdc25
donghoon <adilson8@naver.com>
2023-05-16 17:29:40 +0900

    NumberPlay

--- exercise/src/programmers/lv1/NumberPlay.java
+++ exercise/src/programmers/lv1/NumberPlay.java
@@ -3,143 +3,19 @@ package programmers.lv1;
 public class NumberPlay {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/81301
-	public static void main (String[] args){
-		String s = "one4seveneight";
+	public int solution(String s) {
+        String newS = s.replaceAll("zero", "0")
+		.replaceAll("one", "1")
+		.replaceAll("two", "2")
+		.replaceAll("three", "3")
+		.replaceAll("four", "4")
+		.replaceAll("five", "5")
+		.replaceAll("six", "6")
+		.replaceAll("seven", "7")
+		.replaceAll("eight", "8")
+		.replaceAll("nine", "9");
 		
-		String zeroS = "";
-		if (s.contains("zero")){
-			String[] arr = s.split("zero", -1);
-			String[] newArr = new String[arr.length+1];
-			newArr[0] = arr[0];
-			newArr[1] = "0";
-			newArr[2] = arr[1];
-			for (String e  :newArr){
-				zeroS += e; 
-			}
-			s = zeroS;
-		}
-		
-		String oneS = "";
-		if (s.contains("one")){
-			String[] arr = s.split("one", -1);
-			String[] newArr = new String[arr.length+1];
-			newArr[0] = arr[0];
-			newArr[1] = "1";
-			newArr[2] = arr[1];
-			for (String e  :newArr){
-				oneS += e; 
-			}			
-			s = oneS;
-		}
-		
-		String twoS = "";
-		if (s.contains("two")){
-			String[] arr = s.split("two", -1);
-			String[] newArr = new String[arr.length+1];
-			newArr[0] = arr[0];
-			newArr[1] = "2";
-			newArr[2] = arr[1];
-			for (String e  :newArr){
-				twoS += e; 
-			}		
-			s = twoS;
-		}
-		
-		String threeS = "";
-		if (s.contains("three")){
-			String[] arr = s.split("three", -1);
-			String[] newArr = new String[arr.length+1];
-			newArr[0] = arr[0];
-			newArr[1] = "3";
-			newArr[2] = arr[1];
-			for (String e  :newArr){
-				threeS += e; 
-			}			
-			s = threeS;
-		}
-		
-		String fourS = "";
-		if (s.contains("four")){
-			String[] arr = s.split("four", -1);
-			String[] newArr = new String[arr.length+1];
-			newArr[0] = arr[0];
-			newArr[1] = "4";
-			newArr[2] = arr[1];
-			for (String e  :newArr){
-				fourS += e; 
-			}			
-			s = fourS;
-		}
-		
-		String fiveS = "";
-		if (s.contains("five")){
-			String[] arr = s.split("five", -1);
-			String[] newArr = new String[arr.length+1];
-			newArr[0] = arr[0];
-			newArr[1] = "5";
-			newArr[2] = arr[1];
-			for (String e  :newArr){
-				fiveS += e; 
-			}			
-			s = fiveS;
-		}
-		
-		String sixS = "";
-		if (s.contains("six")){
-			String[] arr = s.split("six", -1);
-			String[] newArr = new String[arr.length+1];
-			newArr[0] = arr[0];
-			newArr[1] = "6";
-			newArr[2] = arr[1];
-			for (String e  :newArr){
-				sixS += e; 
-			}			
-			s = sixS;
-		}
-		
-		String sevenS = "";
-		if (s.contains("seven")){
-			String[] arr = s.split("seven", -1);
-			String[] newArr = new String[arr.length+1];
-			newArr[0] = arr[0];
-			newArr[1] = "7";
-			newArr[2] = arr[1];
-			for (String e  :newArr){
-				sevenS += e; 
-			}			
-			s = sevenS;
-		}
-		
-		String eightS = "";
-		if (s.contains("eight")){
-			String[] arr = s.split("eight", -1);
-			String[] newArr = new String[arr.length+1];
-			newArr[0] = arr[0];
-			newArr[1] = "8";
-			newArr[2] = arr[1];
-			for (String e  :newArr){
-				eightS += e; 
-			}			
-			s = eightS;
-		}
-		
-		String nineS = "";
-		if (s.contains("nine")){
-			String[] arr = s.split("nine", -1);
-			String[] newArr = new String[arr.length+1];
-			newArr[0] = arr[0];
-			newArr[1] = "9";
-			newArr[2] = arr[1];
-			for (String e  :newArr){
-				nineS += e; 
-			}			
-			s = nineS;
-		}
-		
-		System.out.println(s);
-		
-		int answer = Integer.parseInt(s);
-		System.out.println(answer);
-	}
-
+		int answer = Integer.parseInt(newS);
+        return answer;
+    }
 }
