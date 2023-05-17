315a08735d93788ceb586542be3debe4e5108f9c
donghoon <adilson8@naver.com>
2023-05-17 10:05:21 +0900

    NumberK

--- exercise/src/programmers/lv1/NumberK.java
+++ exercise/src/programmers/lv1/NumberK.java
@@ -1,35 +1,24 @@
 package programmers.lv1;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class NumberK {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/42748
-	public static void main (String[] args){
-		int[] array = {1, 5, 2, 6, 3, 7, 4};
-		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-				
-		int[] answer = new int[commands.length];
+	public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
 		
 		for(int i = 0; i < commands.length; i ++){
-			String arrString = "";
-			
-			for (int e : array){
-				arrString += Integer.toString(e);
+			List<Integer> list = new ArrayList<>();
+			for(int j = commands[i][0]-1; j <= commands[i][1]-1; j++){
+				list.add(array[j]);
 			}
-			
-			System.out.println(arrString);
-			char[] charArr = arrString.substring(commands[i][0]-1, commands[i][1]).toCharArray();
-			Arrays.sort(charArr);
-			System.out.println(Arrays.toString(charArr));
-			int answerE = Character.getNumericValue(charArr[commands[i][2]-1]);
-			System.out.println(answerE);
-			System.out.println("==========================");
-			
-			answer[i] = answerE;
+			Collections.sort(list);
+			answer[i] = list.get(commands[i][2]-1);
 		}
-		
-		System.out.println(Arrays.toString(answer));
-	}
-
+        
+        return answer;
+    }
 }
