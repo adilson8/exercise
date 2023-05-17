a3b9b4d896a4b5298ef29fc84841286ecde59657
donghoon <adilson8@naver.com>
2023-05-17 13:44:07 +0900

    Coke

---
+++ exercise/src/programmers/lv1/Coke.java
@@ -0,0 +1,11 @@
+package programmers.lv1;
+
+public class Coke {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/132267
+	public int solution(int a, int b, int n) {
+        int answer = 0;
+        return answer;
+    }
+
+}
