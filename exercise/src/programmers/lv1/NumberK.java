90a0384b9cea23cea080a5ee7bacee610531b81e
donghoon <adilson8@naver.com>
2023-05-17 09:31:45 +0900

    NumberK
    - array의 각 원소는 1 이상 100 이하입니다.

--- exercise/src/programmers/lv1/NumberK.java
+++ exercise/src/programmers/lv1/NumberK.java
@@ -8,14 +8,25 @@ public class NumberK {
 	public static void main (String[] args){
 		int[] array = {1, 5, 2, 6, 3, 7, 4};
 		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-		
+				
 		int[] answer = new int[commands.length];
 		
 		for(int i = 0; i < commands.length; i ++){
-			for (int j = 0; j < commands[i].length; j++){
-				
+			String arrString = "";
+			
+			for (int e : array){
+				arrString += Integer.toString(e);
 			}
-			answer[i] = 0;
+			
+			System.out.println(arrString);
+			char[] charArr = arrString.substring(commands[i][0]-1, commands[i][1]).toCharArray();
+			Arrays.sort(charArr);
+			System.out.println(Arrays.toString(charArr));
+			int answerE = Character.getNumericValue(charArr[commands[i][2]-1]);
+			System.out.println(answerE);
+			System.out.println("==========================");
+			
+			answer[i] = answerE;
 		}
 		
 		System.out.println(Arrays.toString(answer));
