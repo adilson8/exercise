5420c074bbfa5bc9475efee6c4ff4d58d376a141
donghoon <adilson8@naver.com>
2023-05-18 20:31:56 +0900

    Coke 중간커밋

--- exercise/src/programmers/lv1/Coke.java
+++ exercise/src/programmers/lv1/Coke.java
@@ -3,9 +3,22 @@ package programmers.lv1;
 public class Coke {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/132267
-	public int solution(int a, int b, int n) {
-        int answer = 0;
-        return answer;
-    }
+	public static void main (String[] args){
+		int a = 3;
+		int b = 1;
+		int n = 20;
+		
+		boolean keepGoing = true;
+		int answer = 0;
+		while (keepGoing) {
+			int newCoke = 0;
+			newCoke = n/a + n%a;
+			answer += newCoke;
+			System.out.println(newCoke);
+			break;
+		}
+		
+		System.out.println(answer);
+	}
 
 }
