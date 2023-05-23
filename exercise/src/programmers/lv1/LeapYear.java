c3603200d4067766c8ed840f31ddca3a93984188
donghoon <adilson8@naver.com>
2023-05-23 17:23:00 +0900

    LeapYear

---
+++ exercise/src/programmers/lv1/LeapYear.java
@@ -0,0 +1,11 @@
+package programmers.lv1;
+
+public class LeapYear {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12901
+	public String solution(int a, int b) {
+        String answer = "";
+        return answer;
+    }
+
+}
