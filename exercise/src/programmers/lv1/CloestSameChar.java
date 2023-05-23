49d1403fbb9e4d5b4611b290b0d5ef6e774e35fc
donghoon <adilson8@naver.com>
2023-05-23 11:26:48 +0900

    CloestSameChar

---
+++ exercise/src/programmers/lv1/CloestSameChar.java
@@ -0,0 +1,11 @@
+package programmers.lv1;
+
+public class CloestSameChar {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/142086
+	public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+	
+}
