7751c0d430a3cf3e0222509c79ca79f26fccb311
donghoon <adilson8@naver.com>
2023-05-23 15:15:53 +0900

    GoodMemories

---
+++ exercise/src/programmers/lv1/GoodMemories.java
@@ -0,0 +1,11 @@
+package programmers.lv1;
+
+public class GoodMemories {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/176963
+	public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = {};
+        return answer;
+    }
+
+}
