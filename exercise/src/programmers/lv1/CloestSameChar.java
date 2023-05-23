29082db5c6c2765d7396cb1297c649399cf5032d
donghoon <adilson8@naver.com>
2023-05-23 14:47:18 +0900

    CloestSameChar

--- exercise/src/programmers/lv1/CloestSameChar.java
+++ exercise/src/programmers/lv1/CloestSameChar.java
@@ -4,7 +4,24 @@ public class CloestSameChar {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/142086
 	public int[] solution(String s) {
-        int[] answer = {};
+        char[] arrS = s.toCharArray();
+		
+		int[] answer = new int[arrS.length];
+		for(int i = 0; i < arrS.length; i++){
+			if (i == 0){
+				answer[i] = -1;
+			} else {
+				for (int j = 0; j < i; j++){
+					if (arrS[i] == arrS[j]){
+						answer[i] = (j-i)*-1;
+					} 
+				}
+				
+				if(answer[i] == 0){
+					answer[i] = -1;
+				}
+			}
+		}
         return answer;
     }
 	
