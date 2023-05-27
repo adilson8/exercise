65c4bd836965ab9a09c2a65b74bb48a9eb4bb166
adilson8 <adilson8@naver.com>
2023-05-27 19:25:13 +0900

    LeapYear

--- exercise/src/programmers/lv1/LeapYear.java
+++ exercise/src/programmers/lv1/LeapYear.java
@@ -5,6 +5,9 @@ public class LeapYear {
 	// https://school.programmers.co.kr/learn/courses/30/lessons/12901
 	public String solution(int a, int b) {
         String answer = "";
+        
+        String[] day = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        
         return answer;
     }
 
