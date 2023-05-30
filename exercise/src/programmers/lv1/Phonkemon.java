ddff0d5fa45e1d2bf14e02d83e58dfd77267d41c
donghoon <adilson8@naver.com>
2023-05-30 10:53:55 +0900

    Phonkemon

--- exercise/src/programmers/lv1/Phonkemon.java
+++ exercise/src/programmers/lv1/Phonkemon.java
@@ -1,10 +1,35 @@
 package programmers.lv1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Phonkemon {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/1845
-	public int solution(int[] nums) {
-        int answer = 0;
-        return answer;
-    }
+	public static void main (String[] args){
+		int[] nums = {3,3,3,2,2,2};
+		Arrays.sort(nums);
+		System.out.println(Arrays.toString(nums));
+		
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (i == 0){
+				list.add(nums[i]);
+			} else if (list.get(list.size()-1) != nums [i]){
+				list.add(nums[i]);
+			}				
+		}
+		
+		System.out.println(list.toString());
+		
+		int answer = 0;
+		if (list.size() > nums.length / 2){
+			answer = nums.length / 2;
+		} else {
+			answer = list.size();
+		}
+		
+		System.out.println(answer);
+	}
 }
