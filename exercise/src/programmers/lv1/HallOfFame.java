00059103fef9830d8339ebe9b75523ccb9846895
donghoon <adilson8@naver.com>
2023-05-30 13:14:58 +0900

    HallOfFame

--- exercise/src/programmers/lv1/HallOfFame.java
+++ exercise/src/programmers/lv1/HallOfFame.java
@@ -1,10 +1,15 @@
 package programmers.lv1;
 
+import java.util.Arrays;
+
 public class HallOfFame {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/138477
-	public int[] solution(int k, int[] score) {
-        int[] answer = {};
-        return answer;
-    }
+	public static void main (String[] args){
+		int k = 3;
+		int[] score = {10, 100, 20, 150, 1, 100, 200};
+		
+		int[] answer = {};
+		System.out.println(Arrays.toString(answer));
+	}
 }
