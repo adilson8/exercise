4c2e441371a632e461d3db2b256e50467e068543
donghoon <adilson8@naver.com>
2023-05-30 10:59:51 +0900

    HallOfFame

---
+++ exercise/src/programmers/lv1/HallOfFame.java
@@ -0,0 +1,10 @@
+package programmers.lv1;
+
+public class HallOfFame {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/138477
+	public int[] solution(int k, int[] score) {
+        int[] answer = {};
+        return answer;
+    }
+}
