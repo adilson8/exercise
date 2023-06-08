2d465854146a2f3fc70f28ba2ff2d3d3c7665b1d
donghoon <adilson8@naver.com>
2023-06-08 11:21:04 +0900

    HallOfFame

--- exercise/src/programmers/lv1/HallOfFame.java
+++ exercise/src/programmers/lv1/HallOfFame.java
@@ -8,8 +8,25 @@ public class HallOfFame {
 	public static void main (String[] args){
 		int k = 3;
 		int[] score = {10, 100, 20, 150, 1, 100, 200};
+		System.out.println(Arrays.toString(score));
 		
-		int[] answer = {};
+		int[] hallOfFame = {2001, 2001, 2001};
+		int[] answer = new int [score.length];
+		for (int i = 0; i < score.length; i++){
+			if (i < 3) {
+				hallOfFame[i] = score[i];
+				Arrays.sort(hallOfFame);
+			} else {
+				for (int j = 0; j < hallOfFame.length; j++){
+					
+				}
+			}
+			
+			System.out.println(Arrays.toString(hallOfFame));
+			answer[i] = hallOfFame[0];
+		}
+		
+		
 		System.out.println(Arrays.toString(answer));
 	}
 }
