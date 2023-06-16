84ff349bf9f790a983ac42d13ff84df8f21fd1f3
donghoon <adilson8@naver.com>
2023-06-16 14:12:25 +0900

    HallOfFame

--- exercise/src/programmers/lv1/HallOfFame.java
+++ exercise/src/programmers/lv1/HallOfFame.java
@@ -5,30 +5,31 @@ import java.util.Arrays;
 public class HallOfFame {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/138477
-	public static void main (String[] args){
-		int k = 3;
-		int[] score = {10, 100, 20, 150, 1, 100, 200};
-		System.out.println(Arrays.toString(score));
-		
-		int[] hallOfFame = {2001, 2001, 2001};
-		int[] answer = new int [score.length];
-		for (int i = 0; i < score.length; i++){
-			if (i < 3) {
-				hallOfFame[i] = score[i];
-				Arrays.sort(hallOfFame);
-			} else {
-				for (int j = hallOfFame.length; j <= 0; j--){
-					if (score[i] > hallOfFame[j]){
-						// 여기에 작업 
-					}
-				}
-			}
-			
-			System.out.println(Arrays.toString(hallOfFame));
-			answer[i] = hallOfFame[0];
-		}
-		
-		
-		System.out.println(Arrays.toString(answer));
-	}
+	public int[] solution(int k, int[] score) {
+
+        int[] hallOfFame = new int[k];
+        for (int i = 0; i < k; i++){
+            hallOfFame[i] = 2001;
+        }
+
+        int[] answer = new int [score.length];
+        for (int i = 0; i < score.length; i++){
+            if (i < k) {
+                hallOfFame[i] = score[i];
+                Arrays.sort(hallOfFame);
+            } else {
+                for(int j = 0; j < hallOfFame.length; j++){
+                    if (hallOfFame[j]<score[i]){
+                        hallOfFame[j] = score[i];
+                        Arrays.sort(hallOfFame);
+                        break;
+                    }
+                }
+            }
+
+            answer[i] = hallOfFame[0];
+        }
+
+        return answer;
+    }
 }
