2a45de45b2c57aeaf77db14b5c7442cd8300de07
donghoon <adilson8@naver.com>
2023-06-19 15:41:15 +0900

    MockExam

---
+++ exercise/src/programmers/lv1/MockExam.java
@@ -0,0 +1,12 @@
+package programmers.lv1;
+
+public class MockExam {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/42840
+	public static void main(String[] args) {
+		int[] answers = {};
+		int[] answer = {};
+        System.out.println( answer);
+	}
+
+}
