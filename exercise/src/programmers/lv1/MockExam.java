9c1b60e07e7aa02c0f7de16437b000ded57f0995
donghoon <adilson8@naver.com>
2023-06-20 21:02:12 +0900

    MockExam

--- exercise/src/programmers/lv1/MockExam.java
+++ exercise/src/programmers/lv1/MockExam.java
@@ -3,10 +3,22 @@ package programmers.lv1;
 public class MockExam {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/42840
-	public static void main(String[] args) {
-		int[] answers = {};
+	public static void main (String[] args){
+
+		int[] answers = {1,2,3,4,5};
+		
+		// 1번수포자 : 12345
+		// 2번수포자 : 21232425
+		// 3번수포자 : 3311224455
+		
+		for (int i = 0; i < answers.length; i++){
+			
+		}
+		
+		
 		int[] answer = {};
-        System.out.println( answer);
+        System.out.println(answer);
+        
 	}
 
 }
