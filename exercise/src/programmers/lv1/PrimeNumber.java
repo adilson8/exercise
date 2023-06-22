7ecfa1ec7472656257cf6f7f5796e5b323dac2af
adilson8 <adilson8@naver.com>
2023-06-22 16:20:55 +0900

    PrimeNumber

--- exercise/src/programmers/lv1/PrimeNumber.java
+++ exercise/src/programmers/lv1/PrimeNumber.java
@@ -1,11 +1,24 @@
 package programmers.lv1;
 
+import java.util.Arrays;
+
 public class PrimeNumber {
 
 	// https://school.programmers.co.kr/learn/courses/30/lessons/12977
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] nums = {1,2,3,4};
+		
+		for (int i = 0; i < nums.length-2; i++) {
+			for (int j = i+1; j < nums.length-1; j++) {
+				for (int k = i+2; k < nums.length; k++) {
+					int[] test = new int[3];
+					test[0] = nums[i];
+					test[1] = nums[j];
+					test[2] = nums[k];
+					System.out.println(Arrays.toString(test));
+				}
+			}
+		}
 	}
 
 }
