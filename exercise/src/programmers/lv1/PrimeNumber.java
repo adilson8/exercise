cce76242c4e84b65774c138d8a5980f3da429b98
adilson8 <adilson8@naver.com>
2023-06-22 15:46:55 +0900

    PrimeNumber

---
+++ exercise/src/programmers/lv1/PrimeNumber.java
@@ -0,0 +1,11 @@
+package programmers.lv1;
+
+public class PrimeNumber {
+
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12977
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
