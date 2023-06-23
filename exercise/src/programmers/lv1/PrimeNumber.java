88c57701b0a19fd720c319df6ebfe7f512e78c85
adilson8 <adilson8@naver.com>
2023-06-23 10:10:06 +0900

    PrimeNumber

--- exercise/src/programmers/lv1/PrimeNumber.java
+++ exercise/src/programmers/lv1/PrimeNumber.java
@@ -1,38 +1,46 @@
 package programmers.lv1;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class PrimeNumber {
 
 	// https://school.programmers.co.kr/learn/courses/30/lessons/12977
 	public static void main(String[] args) {
-		int[] nums = {1,2,3,4};
+		int[] nums = {1,2,7,6,4};
 		
-//		for (int i = 0; i < nums.length-2; i++) {
-//			for (int j = i+1; j < nums.length-1; j++) {
-//				for (int k = i+2; k < nums.length; k++) {
-//					int[] test = new int[3];
-//					test[0] = nums[i];
-//					test[1] = nums[j];
-//					test[2] = nums[k];
-//					System.out.println(Arrays.toString(test));
-//				}
-//			}
-//		}
+		List<Integer> sumList = new ArrayList<>();
+		for (int i = 0; i < nums.length-2; i++) {
+			for (int j = i+1; j < nums.length-1; j++) {
+				for (int k = i+2; k < nums.length; k++) {
+					int sum = nums[i] + nums[j] + nums[k];
+					sumList.add(sum);
+				}
+			}
+		}
+		
+		sumList.sort(Comparator.naturalOrder());
+		List<Integer> sumList2 = new ArrayList<>();
+		for (int i = 0; i < sumList.size(); i++) {
+			if (i == 0) {
+				sumList2.add(sumList.get(i));
+			} else {
+				if (sumList2.get(sumList2.size()-1) != sumList.get(i)) {
+					sumList2.add(sumList.get(i));
+				}
+			}
+		}
+		
+		System.out.println(sumList2.toString());
+		int answer = 0;
+		for (int n : sumList2) {
+			if (PrimeNumber.isPrimeNum(n)) {
+				answer++;
+			}
+		}
 
-//		int test = 3;
-//		boolean isPrimeNum = true;
-//		for (int i = 2; i < test; i++) {
-//			if (test % i == 0) {
-//				System.out.println("소수가 아니야!!!!");
-//				isPrimeNum = false;
-//				break;
-//			}
-//		}
-//		System.out.println( test+ "은(는) 소수인가요? : " + isPrimeNum);
-		
-		PrimeNumber.isPrimeNum(8);
-		
+		System.out.println(answer);
 	}
 	
 	public static boolean isPrimeNum (int n) {
