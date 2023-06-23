71dc09d35f58d583542dc49869ea0fa8568968f9
adilson8 <adilson8@naver.com>
2023-06-23 10:25:52 +0900

    PrimeNumber

--- exercise/src/programmers/lv1/PrimeNumber.java
+++ exercise/src/programmers/lv1/PrimeNumber.java
@@ -6,40 +6,38 @@ import java.util.List;
 public class PrimeNumber {
 
 	// https://school.programmers.co.kr/learn/courses/30/lessons/12977
-	public static void main(String[] args) {
-		int[] nums = {1,2,7,6,4};
-		
-		List<Integer> sumList = new ArrayList<>();
-		for (int i = 0; i < nums.length-2; i++) {
-			for (int j = i+1; j < nums.length-1; j++) {
-				for (int k = j+1; k < nums.length; k++) {
-					int sum = nums[i] + nums[j] + nums[k];
-					sumList.add(sum);
-				}
-			}
-		}
-		
-		int answer = 0;
-		for (int n : sumList) {
-			if (PrimeNumber.isPrimeNum(n)) {
-				answer++;
-			}
-		}
+	public int solution(int[] nums) {
+        List<Integer> sumList = new ArrayList<>();
+        for (int i = 0; i < nums.length-2; i++) {
+            for (int j = i+1; j < nums.length-1; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    sumList.add(sum);
+                }
+            }
+        }
 
-		System.out.println(answer);
-	}
-	
-	public static boolean isPrimeNum (int n) {
-		boolean isPrimeNum = true;
-		
-		for (int i = 2; i < n; i++) {
-			if (n % i == 0) {
-				isPrimeNum = false;
-				break;
-			}
-		}
-		
-		return isPrimeNum;
-	}
+        int answer = 0;
+        for (int n : sumList) {
+            if (PrimeNumber.isPrimeNum(n)) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static boolean isPrimeNum (int n) {
+        boolean isPrimeNum = true;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                isPrimeNum = false;
+                break;
+            }
+        }
+
+        return isPrimeNum;
+    }
 
 }
