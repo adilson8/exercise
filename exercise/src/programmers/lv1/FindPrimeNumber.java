617d09c60c152ff88a281c6ccb4a44eae0290247
adilson8 <adilson8@naver.com>
2023-06-23 10:29:14 +0900

    FindPrimeNumber

---
+++ exercise/src/programmers/lv1/FindPrimeNumber.java
@@ -0,0 +1,12 @@
+package programmers.lv1;
+
+public class FindPrimeNumber {
+
+	public static void main(String[] args) {
+		int n = 10;
+		int answer = 0;
+		System.out.println(answer);
+		
+	}
+
+}
