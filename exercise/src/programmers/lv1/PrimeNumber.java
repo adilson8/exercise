cf0d200d2956c6d5f1f83a780777d287951a894f
adilson8 <adilson8@naver.com>
2023-06-23 10:24:19 +0900

    PrimeNumber

--- exercise/src/programmers/lv1/PrimeNumber.java
+++ exercise/src/programmers/lv1/PrimeNumber.java
@@ -1,7 +1,6 @@
 package programmers.lv1;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class PrimeNumber {
@@ -13,28 +12,15 @@ public class PrimeNumber {
 		List<Integer> sumList = new ArrayList<>();
 		for (int i = 0; i < nums.length-2; i++) {
 			for (int j = i+1; j < nums.length-1; j++) {
-				for (int k = i+2; k < nums.length; k++) {
+				for (int k = j+1; k < nums.length; k++) {
 					int sum = nums[i] + nums[j] + nums[k];
 					sumList.add(sum);
 				}
 			}
 		}
 		
-		sumList.sort(Comparator.naturalOrder());
-		List<Integer> sumList2 = new ArrayList<>();
-		for (int i = 0; i < sumList.size(); i++) {
-			if (i == 0) {
-				sumList2.add(sumList.get(i));
-			} else {
-				if (sumList2.get(sumList2.size()-1) != sumList.get(i)) {
-					sumList2.add(sumList.get(i));
-				}
-			}
-		}
-		
-		System.out.println(sumList2.toString());
 		int answer = 0;
-		for (int n : sumList2) {
+		for (int n : sumList) {
 			if (PrimeNumber.isPrimeNum(n)) {
 				answer++;
 			}
@@ -44,8 +30,8 @@ public class PrimeNumber {
 	}
 	
 	public static boolean isPrimeNum (int n) {
-		
 		boolean isPrimeNum = true;
+		
 		for (int i = 2; i < n; i++) {
 			if (n % i == 0) {
 				isPrimeNum = false;
