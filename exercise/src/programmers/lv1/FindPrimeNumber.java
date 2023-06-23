d771e585d2dfc3e71e83d9580ecda215b2719c7f
adilson8 <adilson8@naver.com>
2023-06-23 16:59:13 +0900

    FindPrimeNumber

--- exercise/src/programmers/lv1/FindPrimeNumber.java
+++ exercise/src/programmers/lv1/FindPrimeNumber.java
@@ -2,11 +2,24 @@ package programmers.lv1;
 
 public class FindPrimeNumber {
 
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12921
 	public static void main(String[] args) {
 		int n = 10;
-		int answer = 0;
-		System.out.println(answer);
 		
+		int noPrime = 1;
+
+		for (int i = 3; i <= n; i++) {
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					noPrime++;
+					break;
+				}
+			}
+		}
+		
+		int answer = n - noPrime;
+		
+		System.out.println(answer);
 	}
 
 }
