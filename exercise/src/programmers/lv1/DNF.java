4c3f4092fdb267a207e955b51de2a34f1f1e24fa
donghoon <adilson8@naver.com>
2024-07-31 09:15:43 +0900

    DNF 시간초과 해결

--- exercise/src/programmers/lv1/DNF.java
+++ exercise/src/programmers/lv1/DNF.java
@@ -1,29 +1,27 @@
 package programmers.lv1;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class DNF {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/42576
 	public String solution(String[] participant, String[] completion) {
-        String answer = "";
-		List<String> participantList = Arrays.asList(participant);
-		List<String> completiontList = Arrays.asList(completion);
+		String answer = "";
 		
-		for (String p : participant){
-			for (String c : completion){
-				if (p.equals(c)) {
-					participantList.set(participantList.indexOf(c), " ");
-					completiontList.set(completiontList.indexOf(c), " ");
-				}
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		
+		for (int i = 0; i < participant.length-1; i++){
+			if (!participant[i].equals(completion[i])){
+				answer = participant[i];
+				break;
 			}
 		}
 		
-		Collections.sort(participantList, Collections.reverseOrder());
-		
-		answer = participantList.get(0);
+		if (answer.equals("")){
+			answer = participant[participant.length-1];
+		}
+        
         return answer;
     }
 
