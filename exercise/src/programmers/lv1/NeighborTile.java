9147f94f161e1e769ce9d901e222f5b9469e4c5c
donghoon <adilson8@naver.com>
2024-09-06 10:22:17 +0900

    NeighborTile

--- exercise/src/programmers/lv1/NeighborTile.java
+++ exercise/src/programmers/lv1/NeighborTile.java
@@ -1,70 +1,28 @@
 package programmers.lv1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class NeighborTile {
 
 	// https://school.programmers.co.kr/learn/courses/30/lessons/250125
-	public static void main(String[] args) {
-		 
-		// answer : 2; 
-		String[][] board = {
-			    {"blue", "red", "orange", "red"},
-			    {"red", "red", "blue", "orange"},
-			    {"blue", "orange", "red", "red"},
-			    {"orange", "orange", "red", "blue"}
-			};
-		int h = 1;
-		int w = 1;
-		
-		// answer : 1; 
-//		String[][] board = {
-//			    {"yellow", "green", "blue"},
-//			    {"blue", "green", "yellow"},
-//			    {"yellow", "blue", "blue"}
-//			};
-//		int h = 0;
-//		int w = 1;
-		
+	public static void main(String[][] board, int h, int w) {
+
 		int answer = 0;
-		String target = board[h][w];
 		
-		String left = "";
-		if(w > 0){
-			left = board[h][w-1];			
-		}
+		int n = board.length;
+		int[] dh = {0, 1, -1, 0};
+		int[] dw = {1, 0, 0, -1};
 		
-		String right = "";
-		if (w+1 < board[h].length-1){
-			right = board[h][w+1];			
-		}
-		
-		String up = "";
-		if (h > 0){
-			up = board[h-1][w];			
-		}
-		
-		String down = "";
-		if (h+1 < board[w].length-1){
-			down = board[h+1][w];			
-		}
-		
-		List<String> neighbors = new ArrayList<>();
-		neighbors.add(left);
-		neighbors.add(right);
-		neighbors.add(up);
-		neighbors.add(down);
-		
-		for (String neighbor : neighbors){
-			if (target.equals(neighbor)) {
-				answer++;
+		for (int i = 0; i < 4; i++){
+			int h_check = h + dh[i];
+			int w_check = w + dw[i];
+			
+			if (h_check >= 0 && h_check < n
+					&& w_check >= 0 && w_check < n){
+				if(board[h][w].equals(board[h_check][w_check])) {
+					answer++;
+				}
 			}
 		}
 		
-		System.out.println(answer);
-		
-
 	}
 
 }
