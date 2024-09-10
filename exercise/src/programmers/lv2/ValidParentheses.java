d24e3fee82a674c7ed89b68f2a20341312146685
donghoon <adilson8@naver.com>
2024-09-10 17:13:21 +0900

    ValidParentheses 런타임에러

--- exercise/src/programmers/lv2/ValidParentheses.java
+++ exercise/src/programmers/lv2/ValidParentheses.java
@@ -2,20 +2,23 @@ package programmers.lv2;
 
 import java.util.Stack;
 
-public class StackAndQueue {
+public class ValidParentheses {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/12909
 	public static void main (String[] args){
 		
 		// answer = false;
-		String s = ")()(";		
+//		String s = ")()(";		
+		
+		String s = "())";		
+		
 		
 		boolean answer;
 		String[] arrS = s.split("");
 		Stack<String> stackS = new Stack<>();
 		String tmp = arrS[0];
 		
-		if (s.length()%2 !=0 ){
+		if (s.length()%2 !=0 || tmp.equals(")")){
 			answer = false;
 		} else {
 			for (String as : arrS){
