f4a3967b77e059b31bc291c97d36b8b98195baf7
donghoon <adilson8@naver.com>
2024-12-30 10:15:49 +0900

    Bracket

---
+++ exercise/src/programmers/lv2/Bracket.java
@@ -0,0 +1,44 @@
+package programmers.lv2;
+
+public class Bracket {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12985
+	public static void bracket (int n, int a, int b){
+		int answer = 0;
+		
+		while (n > 1){
+			if (a%2 ==0){
+				if (a-b == 1){
+					answer++;
+					break;
+				}
+			}
+			
+			if (b%2 ==0){
+				if (b-a == 1){
+					answer++;
+					break;
+				}
+			}
+			
+			answer++;
+			
+			if (a%2 == 0) {
+				a = a/2;
+			} else {
+				a = (a+1)/2;				
+			}
+			
+			if (b%2 == 0) {
+				b = b/2;
+			} else {
+				b = (b+1)/2;				
+			}			
+			
+			n = n/2;
+		}
+		
+		System.out.println(answer);
+	}
+
+}
