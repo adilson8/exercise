23926123afad157d30b9040d3360038f236426d1
adils <adilson8@naver.com>
2025-01-09 15:09:58 +0900

    commit test

--- exercise/src/CollectionFramework/HashMapExample1.java
+++ exercise/src/CollectionFramework/HashMapExample1.java
@@ -10,7 +10,7 @@ import lombok.Cleanup;
 
 public class HashMapExample1 {
 	public static void main(String[] args) {
-		@Cleanup("clear")
+//		@Cleanup("clear")
 		// 1. Map 컬렉션 생성
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		
--- exercise/src/CollectionFramework/VectorExample.java
+++ exercise/src/CollectionFramework/VectorExample.java
@@ -8,7 +8,7 @@ import lombok.Cleanup;
 public class VectorExample {
 	
 	public static void main(String[] args) {
-		@Cleanup("clear")
+//		@Cleanup("clear")
 		List<Board> list = new Vector<Board>();
 	
 		list.add(new Board("제목1", "내용1", "글쓴이1"));
