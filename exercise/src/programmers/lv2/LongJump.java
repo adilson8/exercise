37ceeffa061b391edc3a57aaef43ec907a82f403
donghoon <adilson8@naver.com>
2025-01-09 15:34:43 +0900

    LongJump

---
+++ exercise/src/programmers/lv2/LongJump.java
@@ -0,0 +1,17 @@
+package programmers.lv2;
+
+public class LongJump {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12914
+	public static void main (String[] args){
+		
+		//answer = 5;
+		int n = 4;
+		
+		// answer = 3;
+//		int n = 3;
+		
+		System.out.println(5%1234567);
+	}
+
+}
