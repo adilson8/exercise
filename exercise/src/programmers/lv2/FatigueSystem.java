62bef894373c65538e2434a1490349d7dea73477
adils <adilson8@naver.com>
2025-01-17 01:18:06 +0900

    FatigueSystem

---
+++ exercise/src/programmers/lv2/FatigueSystem.java
@@ -0,0 +1,25 @@
+package programmers.lv2;
+
+public class FatigueSystem {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/87946
+	public static void main (String[] args) {
+		
+		// answer = 3;
+		int k = 80;
+		int[][] dungeons = {{80,20}, {50,40}, {30,10}};
+		
+		int answer = 0;
+		int tmp = k;
+		
+		for (int i = 0; i < dungeons.length; i++) {
+				System.out.println(dungeons[i][0]);
+			for (int j = 0; j < 2; j++) {
+//				System.out.println(dungeons[i][j]);
+			}
+		}
+		
+		
+	}
+
+}
