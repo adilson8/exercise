ec4e54e5eed122b5daed75ad29ffd996cc381723
adils <adilson8@naver.com>
2025-01-17 01:19:57 +0900

    LeastCommonMultiple

---
+++ exercise/src/programmers/lv2/LeastCommonMultiple.java
@@ -0,0 +1,19 @@
+package programmers.lv2;
+
+public class LeastCommonMultiple {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12953
+	public static void main (String[] args) {
+		
+		// answer = 6;
+		int[] arr = {1,2,3};
+		
+		// answer = 168;
+//		int[] arr = {2,6,8,14};
+		
+		int answer = 0;
+		
+		System.out.println(answer);
+	}
+
+}
