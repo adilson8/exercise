f4d22397d2c5cc742ae9ee38f56ad7f49f931dc0
donghoon <adilson8@naver.com>
2025-01-17 18:36:25 +0900

    LeastCommonMultiple 정답

--- exercise/src/programmers/lv2/LeastCommonMultiple.java
+++ exercise/src/programmers/lv2/LeastCommonMultiple.java
@@ -1,19 +1,41 @@
 package programmers.lv2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LeastCommonMultiple {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/12953
-	public static void main (String[] args) {
-		
-		// answer = 6;
-		int[] arr = {1,2,3};
-		
-		// answer = 168;
-//		int[] arr = {2,6,8,14};
+	public static int leastCommonMultiple (int[] arr) {
 		
 		int answer = 0;
+
+		List<Integer> list = new ArrayList<>(); 
+		for (int e : arr){
+			list.add(e);
+		}
+		Collections.sort(list);
 		
-		System.out.println(answer);
+		boolean tmp = true;
+		int i = 0;
+		
+		while (tmp){
+			i++;
+			int multiple = list.get(list.size()-1) * i;
+		
+			for (int j = 0; j < list.size(); j++){
+				if (multiple % list.get(j) == 0){
+					tmp = false;
+				} else {
+					tmp = true;
+					break;
+				}
+			}
+		}
+		
+		answer = list.get(list.size()-1) * i;
+		return answer;
 	}
 
 }
