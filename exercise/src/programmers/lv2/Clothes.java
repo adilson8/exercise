3a714b00bf239bcbc62b2007ac7837102ee6b548
adilson8 <adilson8@naver.com>
2025-02-03 13:02:19 +0900

    Clothes

---
+++ exercise/src/programmers/lv2/Clothes.java
@@ -0,0 +1,17 @@
+package programmers.lv2;
+
+public class Clothes {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/42578
+	public static void main (String[] args) {
+		// answer = 5;
+		String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+		
+		// answer = 3;
+//		String[][] clothes = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+		
+		
+		
+	}
+
+}
