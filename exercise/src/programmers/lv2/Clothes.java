a149f2fdf63951e0e6748a4eb61c407ad2b0fb2d
adilson8 <adilson8@naver.com>
2025-02-03 18:02:14 +0900

    Clothes 정답

--- exercise/src/programmers/lv2/Clothes.java
+++ exercise/src/programmers/lv2/Clothes.java
@@ -1,17 +1,38 @@
 package programmers.lv2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Clothes {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/42578
-	public static void main (String[] args) {
-		// answer = 5;
-		String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+	public static int main (String[][] clothes) {		
+		int answer = 1;
 		
-		// answer = 3;
-//		String[][] clothes = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+		Map<String, List<String>> clothesMap = new HashMap<>();
+		for (int i = 0; i < clothes.length; i++) {
+			if (clothesMap.containsKey(clothes[i][1])) {
+				clothesMap.get(clothes[i][1]).add(clothes[i][0]);
+			} else {
+				List<String> list = new ArrayList<>();
+				list.add(clothes[i][0]);
+				clothesMap.put(clothes[i][1], list);
+			}
+		}
 		
+		if (clothesMap.size() == 1) {
+//			answer = clothesMap.get(clothesMap.keySet().iterator().next()).size();
+			answer = clothesMap.entrySet().iterator().next().getValue().size();
+		} else {
+			for (String key : clothesMap.keySet()) {
+				answer *= (clothesMap.get(key).size()+1);
+			}
+			answer -= 1;
+		}
 		
-		
+		return answer;
 	}
 
 }
