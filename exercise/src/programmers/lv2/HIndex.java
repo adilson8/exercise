b25fa998d05ec717ef220a84b5e12abc00534d95
adilson8 <adilson8@naver.com>
2025-02-04 13:00:42 +0900

    H-Index 정답

--- exercise/src/programmers/lv2/HIndex.java
+++ exercise/src/programmers/lv2/HIndex.java
@@ -1,39 +1,29 @@
 package programmers.lv2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class HIndex {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/42747
-	public static void main (String[] args) {
-		
-		// answer = 3
-//		int[] citations = {3, 0, 6, 1, 5};
-		
-		// answer = 4
-		int[] citations = {5, 6, 7, 8};
-				
+	public static int main (int[] citations) {
 		List<Integer> citationsList = new ArrayList<>();
 		for (int c :citations) {
 			citationsList.add(c);
-		}
-		
+		}		
 		Collections.sort(citationsList, Collections.reverseOrder());
 		
 		int answer = 0;
 		for (int i = 0; i < citationsList.size(); i++) {
-			if (i >= citationsList.get(i)) {
-				answer = i;
-				break;
-			}
+			if (citationsList.get(i) >= i+1) {
+				answer = i+1;
+            } else {
+                break;
+            }			
 		}
 		
-		System.out.println(answer);
-		System.out.println(citationsList);
-		
+		return answer;
 	}
 
 }
