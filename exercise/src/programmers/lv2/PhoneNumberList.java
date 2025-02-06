592a9c6d34e6831a66959f0cc0115cdfa388cf88
adilson8 <adilson8@naver.com>
2025-02-06 13:06:54 +0900

    PhoneNumberList 시간초과

--- exercise/src/programmers/lv2/PhoneNumberList.java
+++ exercise/src/programmers/lv2/PhoneNumberList.java
@@ -1,6 +1,8 @@
 package programmers.lv2;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PhoneNumberList {
 	
@@ -16,21 +18,27 @@ public class PhoneNumberList {
 		// answer = false
 //		String[] phone_book = {"12","123","1235","567","88"};
 		
-		Arrays.sort(phone_book);
-		
 		boolean answer = true;
-		Loop1 :
-		for(int i = 0; i < phone_book.length; i++){
-			String compare = phone_book[i];
-			Loop2 :
-			for (int j = i+1; j < phone_book.length; j++) {
-				String compare2 = phone_book[j];
-				if (compare2.length() >= compare.length() && compare2.startsWith(compare)) {
-					answer = false;
-					break Loop1;
-				}
-			}
-		}
+		
+		Arrays.sort(phone_book);
+		Map<String, String> phoneHash = new HashMap<>();
+//		for 배열돌기
+//		if phoneHash.get(phoneNum)==null 이면 넣고(value에는 가라값 넣어도되나), 아니면 false
+		
+		
+//		Arrays.sort(phone_book);
+//		Loop1 :
+//		for(int i = 0; i < phone_book.length; i++){
+//			String compare = phone_book[i];
+//			Loop2 :
+//			for (int j = i+1; j < phone_book.length; j++) {
+//				String compare2 = phone_book[j];
+//				if (compare2.length() >= compare.length() && compare2.startsWith(compare)) {
+//					answer = false;
+//					break Loop1;
+//				}
+//			}
+//		}
 		
 		System.out.println(answer);		
 	}
