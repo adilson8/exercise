a985f57e6672fef0d09ae36cf3a6fbb0245ce962
donghoon <adilson8@naver.com>
2025-02-12 17:48:11 +0900

    Process 정답

--- exercise/src/programmers/lv2/Process.java
+++ exercise/src/programmers/lv2/Process.java
@@ -1,28 +1,16 @@
 package programmers.lv2;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 
 public class Process {
 	
 	// https://school.programmers.co.kr/learn/courses/30/lessons/42587
-	public static void main (String[] args) {
-//		int[] priorities, int location
-		
-		// answer = 1
-		int[] priorities = {2, 1, 3, 2};
-		int location = 2; // 2번쨰 인덱스가 몇번째로 실행되는지?
-		
-		// answer = 5
-//		int[] priorities = {1, 1, 9, 1, 1, 1};
-//		int location = 0;
-		
-		int answer = 0;
+	public static int main (int[] priorities, int location) {
+		int answer = 1;
 		
 		Queue<Integer> pQueue  = new LinkedList<>();
-		Queue<Integer> idxQueue = new LinkedList<>(); // 인덱스 큐
+		Queue<Integer> idxQueue = new LinkedList<>();
 		int maxNum = 0;
 		
 		for (int i = 0; i < priorities.length; i++) {
@@ -31,16 +19,24 @@ public class Process {
             maxNum = Math.max(maxNum, priorities[i]);
         }
 		
-		// 이제 location 값인 2번째 인덱스가 몇번째로 실행되는지 answer에 저장하는 로직 추가필요
 		while (pQueue.size()!=0) {
 			if (pQueue.peek()<maxNum) {
 				pQueue.add(pQueue.peek());
 				pQueue.poll();
-				// 여기에 idxQueue 처리
 				
+				// idxQueue 처리
+				idxQueue.add(idxQueue.peek());
+				idxQueue.poll();
 			} else {
-				pQueue.poll();				
-				// 여기에 idxQueue 처리
+				pQueue.poll();
+				
+				if (idxQueue.peek()==location){
+					break;
+				} else {
+					// idxQueue 처리
+					idxQueue.poll();
+					answer++;
+				}
 				
 				maxNum = 0;
 				for (int a : pQueue) {
@@ -49,7 +45,7 @@ public class Process {
 			}
 		}
 			
-		System.out.println(answer);
+		return answer;
 		
 	}
 
