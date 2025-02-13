1eab6340098801f3eb8e7b6d09d743fd6060e571
adilson8 <adilson8@naver.com>
2025-02-13 17:32:24 +0900

    LongJump 규칙발견

--- exercise/src/programmers/lv2/LongJump.java
+++ exercise/src/programmers/lv2/LongJump.java
@@ -6,12 +6,31 @@ public class LongJump {
 	public static void main (String[] args){
 		
 		//answer = 5;
-		int n = 4;
+//		int n = 4;
 		
 		// answer = 3;
 //		int n = 3;
 		
-		System.out.println(5%1234567);
+		int n = 7;
+		// n이 1 : 1가지
+		// n이 2 : 2가지
+		// n이 3 : 3가지
+		// n이 4 : 5가지
+		// n이 5 : 8가지
+		// n이 6 : 13가지
+		// n이 7 : 21가지
+		
+		int answer = 0;
+		int n1 = 1;
+		int n2 = 2;
+		
+		for (int i = 3; i < n+1; i++) {
+			answer = n1 + n2;
+			n1 = n2;
+			n2 = answer;
+		}
+		
+		System.out.println(answer%1234567);
 	}
 
 }
