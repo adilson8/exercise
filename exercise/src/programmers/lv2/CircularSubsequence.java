3d3fcc2bfb25b3f5eae2ebd9222920227d432495
donghoon <adilson8@naver.com>
2025-02-14 12:28:12 +0900

    CircularSubsequence

---
+++ exercise/src/programmers/lv2/CircularSubsequence.java
@@ -0,0 +1,14 @@
+package programmers.lv2;
+
+public class CircularSubsequence {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/131701
+	public static void main (String[] args){
+		
+		// answer = 18
+		int[] elements = {7,9,1,1,4};
+
+		
+	}
+
+}
