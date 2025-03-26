91e2076e4270b9717eca26a583ac590378315d57
donghoon <adilson8@naver.com>
2025-03-26 14:44:43 +0900

    RollcakeCutting

---
+++ exercise/src/programmers/lv2/RollcakeCutting.java
@@ -0,0 +1,16 @@
+package programmers.lv2;
+
+public class RollcakeCutting {
+	
+	public static void main(String[] args){
+		
+		// answer = 2
+		int[] topping = {1, 2, 1, 3, 1, 4, 1, 2};
+		
+		// answer = 0
+//		int[] topping = {1, 2, 3, 1, 4};
+		
+		
+	}
+
+}
