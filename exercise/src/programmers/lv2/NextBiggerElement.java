dd3183d0ffb64dedeeafc72d4ee13c3b40ba45f8
donghoon <adilson8@naver.com>
2025-03-26 15:31:02 +0900

    NextBiggerElement

---
+++ exercise/src/programmers/lv2/NextBiggerElement.java
@@ -0,0 +1,19 @@
+package programmers.lv2;
+
+public class NextBiggerElement {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/154539
+	public static void main (String[] args){
+		
+		// answer = {3, 5, 5, -1}
+		int[] numbers = {2, 3, 3, 5};
+		
+		// answer = {-1, 5, 6, 6, -1, -1}
+//		int[] numbers = {9, 1, 5, 3, 6, 2};
+		
+		int[] answer = new int[numbers.length];
+		
+				
+	}
+
+}
