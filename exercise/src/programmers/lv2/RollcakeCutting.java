3069c76a3626f1e777f4f704e59754e3c9e61243
donghoon <adilson8@naver.com>
2025-03-26 14:57:46 +0900

    RollcakeCutting 시간초과

--- exercise/src/programmers/lv2/RollcakeCutting.java
+++ exercise/src/programmers/lv2/RollcakeCutting.java
@@ -1,16 +1,41 @@
 package programmers.lv2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RollcakeCutting {
 	
 	public static void main(String[] args){
 		
 		// answer = 2
-		int[] topping = {1, 2, 1, 3, 1, 4, 1, 2};
+//		int[] topping = {1, 2, 1, 3, 1, 4, 1, 2};
 		
 		// answer = 0
-//		int[] topping = {1, 2, 3, 1, 4};
+		int[] topping = {1, 2, 3, 1, 4};
 		
+		int answer = 0;
 		
+		for(int i = 1; i < topping.length; i++){
+			
+			Set<Integer> sliceA = new HashSet<>();
+			Set<Integer> sliceB = new HashSet<>();			
+
+			// sliceA
+			for(int j = 0; j < i; j++){
+				sliceA.add(topping[j]);
+			}
+			
+			// sliceB
+			for(int j = i; j < topping.length; j++){
+				sliceB.add(topping[j]);				
+			}
+			
+			if (sliceA.size() == sliceB.size()){
+				answer++;
+			}
+		}
+		
+		System.out.println(answer);
 	}
 
 }
