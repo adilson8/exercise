a0976cca558fbcb23dae280c80e44c4e364eeb21
donghoon <adilson8@naver.com>
2025-03-26 17:23:01 +0900

    OvertimeIndex

---
+++ exercise/src/programmers/lv3/OvertimeIndex.java
@@ -0,0 +1,23 @@
+package programmers.lv3;
+
+public class OvertimeIndex {
+	
+	// https://school.programmers.co.kr/learn/courses/30/lessons/12927s
+	public static void main (String[] args){
+		
+		// answer = 12
+		int[] works = {4, 3, 3};
+		int n = 4;
+		
+		// answer = 6
+//		int[] works = {2, 1, 2};
+//		int n = 1;
+		
+		// answer = 0
+//		int[] works = {1, 1};
+//		int n = 3;
+		
+		
+	}
+
+}
