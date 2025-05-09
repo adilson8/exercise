ba804868021795c8cf5269e029f00e1f987b9ebd
donghoon <adilson8@naver.com>
2025-05-09 17:30:34 +0900

    VowelDictionary

---
+++ exercise/src/programmers/lv2/VowelDictionary.java
@@ -0,0 +1,32 @@
+package programmers.lv2;
+
+public class VowelDictionary {
+	
+	//	https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=java
+	public static void main (String args[]){
+		
+		// answer = 6
+		String word = "AAAAE";
+		int answer = 0;
+		
+//		// answer = 10
+//		String word = "AAAE";
+//		int answer = 0;
+//		
+//		// answer = 1563
+//		String word = "I";
+//		int answer = 0;
+//		
+//		// answer = 1189
+//		String word = "EIO";
+//		int answer = 0;
+		
+		
+		final char[] vowels = {'A','E','I','O','U'};
+		
+		
+		
+		System.out.println(answer);
+	}
+
+}
